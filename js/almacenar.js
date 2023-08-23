35c6ef58651e0a4f6bdb5427a669f4322ae184d8
Sebastian Perez <98673108+sebas-developer@users.noreply.github.com>
2023-08-23 08:39:29 -0300

    funcionalidad js agregada

---
+++ js/almacenar.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const itemInput = document.getElementById("item");
+    const addButton = document.getElementById("agregar");
+    const clearButton = document.getElementById("limpiar");
+    const contenedor = document.getElementById("contenedor");
+  
+    // Cargar ítems almacenados al cargar la página
+    const storedItems = JSON.parse(localStorage.getItem("items")) || [];
+    renderItems(storedItems);
+  
+    addButton.addEventListener("click", function () {
+      const newItem = itemInput.value.trim();
+      if (newItem !== "") {
+        storedItems.push(newItem);
+        localStorage.setItem("items", JSON.stringify(storedItems));
+        renderItems(storedItems);
+        itemInput.value = "";
+      }
+    });
+  
+    clearButton.addEventListener("click", function () {
+      localStorage.removeItem("items");
+      renderItems([]);
+    });
+
+    function renderItems(items) {
+        contenedor.innerHTML = "";
+        items.forEach(function (item) {
+          const li = document.createElement("li");
+          li.className = "list-group-item";
+          li.textContent = item;
+          contenedor.appendChild(li);
+        });
+      }
+    });
